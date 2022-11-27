fc77c0d2947ffff131cb2cf624208ad160910046
Jordan Ang <80706374+jordung@users.noreply.github.com>
2022-11-27 18:18:54 +0800

    commit for exercises 9 to 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if(n<0) {
+    return "OOPS"
+  } else {
+    if(typeof(n) === 'string') {
+      n = parseInt(n);
+    }
+    let n1 = 0, n2 = 1, nextTerm;
+    for (let i = 1; i <= n; i++) {
+      nextTerm = n1 + n2;
+      n1 = n2;
+      n2 = nextTerm;
+    };
+    return n1
+  }
+
 
 };
 
